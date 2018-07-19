79ff6a8b186f38da8d759ced399b6d6eec5ae8eb
Avishay Farbstein <avish.far@gmail.com>
2018-07-19 15:37:45 +0300

    only transaction

--- FlaskWithObject/Model/Retail.py
+++ FlaskWithObject/Model/Retail.py
@@ -9,39 +9,5 @@ class SingleTransaction:
        self.startTime = startTime
 
 
-class SingleCustomer:
 
-    def __init__(self,stam,list=None):
-        self.stam = stam
-        if list is None:
-            list = []
-        self.list = list()
-    
-
-    def __str__(self):
-        return ('_'.join(self.list))
-
-    def len(self):
-        return len(self.list)
-
-class Customers:
-
-    def __init__(self, dictOfCustomers):
-        self.dictOfCustomers = dictOfCustomers
-
-    def show(self):
-        for k,v in self.dictOfCustomers.items():
-            print(k, '-', v)
-
-    def GetCustomer(self,key):
-        return self.dictOfCustomers[key]
-
-    def UpdateCustomer(self,key,SingleCustomer):
-        self.dictOfCustomers[key] = SingleCustomer
-
-    def DeleteCustomer(self,key):
-        self.dictOfCustomer
-
-    def Ids(self):
-        return self.dictOfCustomers.keys()
 
